17358172d6fb529d1783f3dc784e53ffc5d96d0b
Mathew Cortez <mcortez121@gmail.com>
2016-06-22 13:21:41 -0400

    Optimized pizza creator
    The code had a set number of pizzas being generated for every type and size of screen, so I changed it so it could create an amount of pizzas based on the height of the screen pizza.html was being run on.

--- views/js/main.js
+++ views/js/main.js
@@ -515,7 +515,9 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+  var windowHeight = window.screen.height;
+  var pizzas = windowHeight/ s * cols;
+  for (var i = 0; i < pizza; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
